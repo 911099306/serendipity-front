43386c579b16ceb823ec3d6f26d775c0e90f69dd
gao <igao107@163.com>
2024-07-04 00:00:47 +0800

    对接后端装配、抽奖接口

--- src/app/pages/lucky/lucky-wheel-page.tsx
+++ src/app/pages/lucky/lucky-wheel-page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 // @ts-ignore
 import {LuckyWheel} from '@lucky-canvas/react'
 
@@ -8,16 +8,12 @@ import {queryRaffleAwardList, randomRaffle} from '@/apis'
 import {RaffleAwardVO} from "@/types/RaffleAwardVO";
 
 export function LuckyWheelPage() {
-
-    const queryParams = new URLSearchParams(window.location.search);
-    const strategyId = Number(queryParams.get('strategyId'));
     const [prizes, setPrizes] = useState([{}])
     const myLucky = useRef()
 
     const [blocks] = useState([
         {padding: '10px', background: '#869cfa', imgs: [{src: "https://bugstack.cn/images/system/blog-03.png"}]}
     ])
-
 
     const [buttons] = useState([
         {radius: '40%', background: '#617df2'},
@@ -31,6 +27,8 @@ export function LuckyWheelPage() {
 
     // 查询奖品列表
     const queryRaffleAwardListHandle = async () => {
+        const queryParams = new URLSearchParams(window.location.search);
+        const strategyId = Number(queryParams.get('strategyId'));
         const result = await queryRaffleAwardList(strategyId);
         const {code, info, data} = await result.json();
         if (code != "0000") {
@@ -53,27 +51,22 @@ export function LuckyWheelPage() {
 
     // 调用随机抽奖
     const randomRaffleHandle = async () => {
+        const queryParams = new URLSearchParams(window.location.search);
+        const strategyId = Number(queryParams.get('strategyId'));
         const result = await randomRaffle(strategyId);
         const {code, info, data} = await result.json();
         if (code != "0000") {
-            window.alert("获取抽奖奖品列表失败 code:" + code + " info:" + info)
+            window.alert("随机抽奖失败 code:" + code + " info:" + info)
             return;
         }
         // 为了方便测试，mock 的接口直接返回 awardIndex 也就是奖品列表中第几个奖品。
-        return data.awardIndex ? data.awardIndex : prizes.findIndex(prize =>
-            //@ts-ignore
-            prize.fonts.some(font => font.id === data.awardId)
-        ) + 1;
+        return data.awardIndex - 1;
     }
-
 
     useEffect(() => {
         queryRaffleAwardListHandle().then(r => {
         });
     }, [])
-
-
-
 
     return <div>
         <LuckyWheel
